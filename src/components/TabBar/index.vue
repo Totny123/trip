5f543c081ffa87d3fd24b5b979eaf7af135a3542
totny <24455370@qq.com>
2022-09-28 23:57:11 +0800

    feat: vant tabBar

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -1,64 +1,28 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabBarData" :key="item.text">
-      <div
-        class="tab-bar-item"
-        :class="{ active: currentIndex === index }"
-        @click="tabBarClick(item.path, index)"
-      >
-        <img
-          :src="
-            getImgURL(currentIndex !== index ? item.imgURL : item.activeImgURL)
-          "
-          alt=""
-        />
-        <span>{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex">
+      <template v-for="item in tabBarData">
+        <van-tabbar-item icon="home-o" :to="item.path">
+          <template #icon="props">
+            <img
+              class="test"
+              :src="getImgURL(props.active ? item.activeImgURL : item.imgURL)"
+              alt=""
+            />
+          </template>
+          <span>{{ item.text }}</span>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
 import tabBarData from "@/assets/data/tabBarData";
 import { getImgURL } from "@/utils";
-import { useRouter } from "vue-router";
 import { ref } from "vue";
 
-const router = useRouter();
 const currentIndex = ref(0);
-
-const tabBarClick = (path, index) => {
-  router.push(path);
-  currentIndex.value = index;
-};
 </script>
 
-<style lang="less" scoped>
-.tab-bar {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  display: flex;
-
-  &-item {
-    height: 48px;
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-    img {
-      width: 36px;
-      height: auto;
-    }
-
-    span {
-      margin-top: 4px;
-    }
-  }
-}
-.active {
-  color: var(--primary-color);
-}
-</style>
+<style lang="less" scoped></style>
