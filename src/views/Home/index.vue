31303439686a14618be752aa0a7d960aec5073d0
totny <24455370@qq.com>
2022-09-29 00:33:55 +0800

    feat: banner

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
     <NavBar />
+    <div class="banner">
+      <img src="@/assets/img/baidu.png" alt="" />
+    </div>
+    <div class="location">
+      <span>广州</span>
+      <div class="position">
+        <span>我的位置</span>
+        <van-icon name="aim" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +20,23 @@ import NavBar from "./components/NavBar.vue";
 
 <style lang="less" scoped>
 .home {
+  .banner {
+    img {
+      max-width: 100%;
+    }
+  }
+  .location {
+    height: 44px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    font-size: 14px;
+    .position {
+      i {
+        margin-left: 4px;
+      }
+    }
+  }
 }
 </style>
